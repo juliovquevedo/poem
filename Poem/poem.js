a2454ee1ff785da786176f4c97cf2823896b37b2
Julio Quevedo <juliovquevedo@gmail.com>
2018-07-11 20:42:53 -0400

    all code is back

--- Poem/poem.js
+++ Poem/poem.js
@@ -1,17 +1,17 @@
-//window.onload = function () {
-//    var appear = setInterval(showLine, 3500);
-//    var line = 0;
-//    
-//    function showLine () {
-//        if (line == 0){
-//            document.getElementsByClassName("show")[line++].style.display = "block";
-//            document.getElementsByClassName("show")[line++].style.display = "block";
-//        }
-//        else{
-//           document.getElementsByClassName("show")[line++].style.display = "initial"; 
-//        }
-//        if (line > 13){
-//            window.clearInterval(appear);
-//        }
-//    }
-//}
+window.onload = function () {
+    var appear = setInterval(showLine, 3500);
+    var line = 0;
+    
+    function showLine () {
+        if (line == 0){
+            document.getElementsByClassName("show")[line++].style.display = "block";
+            document.getElementsByClassName("show")[line++].style.display = "block";
+        }
+        else{
+           document.getElementsByClassName("show")[line++].style.display = "initial"; 
+        }
+        if (line > 13){
+            window.clearInterval(appear);
+        }
+    }
+}
